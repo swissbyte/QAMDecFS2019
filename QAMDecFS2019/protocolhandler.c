7b34f5137d4019ad6b58d01c90d539176ffda86a
philippeppler <philipp.eppler@gmx.ch>
2019-05-31 15:51:33 +0200

    implementation of CRC8

--- QAMDecFS2019/protocolhandler.c
+++ QAMDecFS2019/protocolhandler.c
@@ -50,14 +50,14 @@ xQueueHandle xALDPQueue;									// Queue from Protocolhandler to Main-Task
 SemaphoreHandle_t xGlobalProtocolBuffer_A_Key;			//A-Resource for ucGlobalProtocolBuffer_A
 SemaphoreHandle_t xGlobalProtocolBuffer_B_Key;			//A-Resource for ucGlobalProtocolBuffer_B
 
-/* global Variables 
+/* global Variables */
 uint8_t ucGlobalProtocolBuffer_A[ PROTOCOL_BUFFER_SIZE ] = {};	// Buffer_A from Demodulator to ProtocolTask
 uint8_t ucGlobalProtocolBuffer_B[ PROTOCOL_BUFFER_SIZE ] = {};	// Buffer_B from Demodulator to ProtocolTask
-*/
-/* Debug ( Testpattern ) */
-uint8_t ucGlobalProtocolBuffer_A[ PROTOCOL_BUFFER_SIZE ] = {8,1,6,3,4,5,6,7,8,66,8,1,6,1,2,3,4,5,6,66,8,0,6,6,7,8,9,10,11,66,8,1};	// Buffer_A from Demodulator to ProtocolTask
-uint8_t ucGlobalProtocolBuffer_B[ PROTOCOL_BUFFER_SIZE ] = {6,3,4,5,6,7,8,66,8,1,6,3,4,5,6,7,8,66,8,1,6,3,4,5,6,7,8,66,2,1,0,66};	// Buffer_B from Demodulator to ProtocolTask
 
+/* Debug ( Testpattern )
+uint8_t ucGlobalProtocolBuffer_A[ PROTOCOL_BUFFER_SIZE ] = {8,1,6,3,4,5,6,7,8,42,8,1,6,1,2,3,4,5,6,16,8,0,6,6,7,8,9,10,11,101,8,1};	// Buffer_A from Demodulator to ProtocolTask
+uint8_t ucGlobalProtocolBuffer_B[ PROTOCOL_BUFFER_SIZE ] = {6,3,4,5,6,7,8,66,8,1,6,3,4,5,6,7,8,66,8,1,6,3,4,5,6,7,8,66,2,1,0,66};	// Buffer_B from Demodulator to ProtocolTask
+*/
 
 void vProtocolHandlerTask( void *pvParameters ) {
 	( void ) pvParameters;
@@ -73,6 +73,7 @@ void vProtocolHandlerTask( void *pvParameters ) {
 	uint8_t ucBufferSLDPpayloadInput[ SLDP_PAYLOAD_MAX_SIZE ]= {};		// could be done better maybe
 	uint8_t ucBufferSLDPpayloadInputCounter;
 	uint8_t ucValidData = pdFALSE;
+	uint32_t ulCRC_ErrorCounter = 0;
 	
 	xGlobalProtocolBuffer_A_Key = xSemaphoreCreateMutex();
 	xGlobalProtocolBuffer_B_Key = xSemaphoreCreateMutex();
@@ -80,10 +81,10 @@ void vProtocolHandlerTask( void *pvParameters ) {
 	xALDPQueue = xQueueCreate( ANZ_SEND_QUEUE, sizeof( uint8_t ) );
 
 	
-/* Debugging */
+/* Debugging
 	PORTF.DIRSET = PIN0_bm; //LED1
 	PORTF.OUT = 0x01;
-	
+*/	
 
 	for( ;; ) {
 /* Debugging		
@@ -160,25 +161,33 @@ void vProtocolHandlerTask( void *pvParameters ) {
 			xSLDP_Paket.sldp_payload = &ucBufferSLDPpayloadInput[ 1 ];
 			
 			xALDP_Paket = ( struct ALDP_t_class * )xSLDP_Paket.sldp_payload;
+
+/* Debug
+			uint8_t array[ 256 ]={};
+			memcpy( array, xALDP_Paket->aldp_payload, xALDP_Paket->aldp_hdr_byte_2 );
+*/
 			
 /* calculating CRC8 */
 			
 			uint8_t ucCRC8 = 0;
 			ucCRC8 = xCRC_calc(ucCRC8, xSLDP_Paket.sldp_size );
-			for ( uint8_t i = 0; i <= xSLDP_Paket.sldp_size; i++ ) {
-				ucCRC8 = xCRC_calc(ucCRC8, ucBufferSLDPpayloadInput[ i ] );
+			for ( uint8_t i = 0; i < xSLDP_Paket.sldp_size; i++ ) {
+				uint8_t ucCRCSend = xSLDP_Paket.sldp_payload[ i ];
+				ucCRC8 = xCRC_calc(ucCRC8, ucCRCSend );
 			}
 
-		
-/* Debug */
-			uint8_t array[ 256 ]={};
-			memcpy( array, xALDP_Paket->aldp_payload, xALDP_Paket->aldp_hdr_byte_2 );
+/* checking CRC8 with calculation */
 
-
+			if(ucCRC8 == xSLDP_Paket.sldp_crc8) {
+				
 /* write ALDP Payload into ALDP-Queue */
-			for ( uint8_t i=0; i < xALDP_Paket->aldp_hdr_byte_2; i++ ) {
-				uint8_t ucSendChar = xSLDP_Paket.sldp_payload[ i+2 ];
-				xQueueSend( xALDPQueue, &ucSendChar, portMAX_DELAY );
+				for ( uint8_t i = 0; i < xALDP_Paket->aldp_hdr_byte_2; i++ ) {
+					uint8_t ucSendChar = xSLDP_Paket.sldp_payload[ i+2 ];
+					xQueueSend( xALDPQueue, &ucSendChar, portMAX_DELAY );
+				}				
+			}
+			else {
+				ulCRC_ErrorCounter++;
 			}
 		}
 	}
